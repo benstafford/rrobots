3a8de180b9dcb151e9c59219585c42d70673c525
Jeremy <jcron@virtualhold.com>
2011-10-21 16:09:56 -0400

    Cory and Jeremy space invader

---
+++ VerticalInvader.rb
@@ -0,0 +1,49 @@
+require 'robot'
+
+class VerticalInvader
+   include Robot
+
+  def initialize
+    @reached_west = false
+    @intent_heading = 90
+
+  end
+
+  def tick events
+    if @reached_west == false
+      accelerate 1
+      if heading != 180
+        turn 10 - heading%10
+      end
+    end
+
+    if x <= 100 and heading!=@intent_heading
+      @reached_west = true
+      stop
+      turn 10 - heading%10
+    end
+
+    if gun_heading < 0
+        turn_gun 30
+    end
+    if gun_heading > 0
+        turn_gun -30
+    end
+
+    if (y > 300)
+      fire 0.1
+    end
+
+    if @reached_west and heading==@intent_heading
+        accelerate 1
+    end
+
+    if @reached_west and heading==@intent_heading and heading == 90 and y <= 200
+        @intent_heading = 270
+    end
+    if @reached_west and heading==@intent_heading and heading == 270 and y >= battlefield_height - 100
+        @intent_heading = 90
+    end
+  end
+
+end
